4bc407f7d5dfc4370c2d26a39d09db742d52cc47
Eric Rainey <rainey75@me.com>
2013-03-12 22:03:14 -0400

    Initial commit and README edit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VenueApp::Application.config.secret_token = '54442ef20f1d7ce3f1f97471c8e77e1caec1249a830734bf44557cca06ec53665fb8d740d403a7bea932c0c6a7a586854842528c712e8cf14f569d22e1c127c4'
